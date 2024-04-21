2098ec9763a85d900d30a3c144abeba54e5bed12
Catherine Tushabe <tushcathy@192.168.1.149>
2024-04-21 12:05:31 +0100

    Add post method

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from tasks import tasks
 
 app = FastAPI()
 
+class Task(BaseModel):
+    title: str
+    description: str = ""
+    done: bool = False
+
 @app.get("/todo/api/v1.0/tasks")
 async def get_tasks():
 
@@ -18,6 +23,28 @@ async def get_task(task_id: int):
     if len(task) == 0:
         raise HTTPException(status_code=404, detail="Item not found")
     return {'task': task[0]}
+
+@app.post("/todo/api/v1.0/tasks", status_code=201)
+def create_task(task:Task):
+    if not task.title:
+        raise HTTPException(status_code=400, detail="Title is required")
+    new_task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': task.title,
+        'description': task.description,
+        'done': task.done
+    }
+    tasks.append(new_task)
+    return {"task": new_task}
+
+@app.middleware("http")
+async def validate_content_type(request, call_next):
+    if request.method == 'POST':
+        if "application/json" not in request.headers.get("content-type", ""):
+            raise HTTPException(status_code=415, detail="Unsupported Media Type. Content-Type header must be application/json.")
+    
+    response = await call_next(request)
+    return response
 
 # @app.get("/")
 # async def root():
